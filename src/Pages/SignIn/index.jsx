b1b969b581667adcc16da9c84491184bda7c1100
rafael <rafaelcoelho2711@gmail.com>
2023-11-23 09:36:46 -0300

    useauth hook used on signin

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { Input } from '../../Components/Input'
 import { Link } from "react-router-dom";
 import { Button } from '../../Components/Button'
 import { ButtonText } from '../../Components/ButtonText'
 import { FiMail, FiLock} from "react-icons/fi";
+import { useAuth } from "../../hooks/auth";
 
 export function SignIn(){
+    const { signIn } = useAuth()
+
+    const [ email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSignIn(){
+        signIn({ email, password})
+    }
+
     return(
         <Container>
             <Form>
@@ -13,10 +24,10 @@ export function SignIn(){
                 <p>Application to follow everything you watch.</p>
                 <h2>Log In</h2>
 
-                <Input icon={FiMail} type="text" placeholder="E-mail"/>
-                <Input icon={FiLock} type="password" placeholder="Passowrd"/>
+                <Input icon={FiMail} type="text" placeholder="E-mail" onChange={e => setEmail(e.target.value)}/>
+                <Input icon={FiLock} type="password" placeholder="Passowrd" onChange={e => setPassword(e.target.value)}/>
 
-                <Button title="Enter"/>
+                <Button title="Enter" onClick={handleSignIn}/>
 
                 <ButtonText>
                    <Link to="/register">
