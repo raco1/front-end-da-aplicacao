12d6e9231c9a21715826e4c5d46ecd05f3bea04e
rafael <rafaelcoelho2711@gmail.com>
2023-11-23 14:48:14 -0300

    header and button redefined

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,25 +1,37 @@
-import { Container, Profile } from "./styles";
+import { Container, Profile, Search } from "./styles";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
 
-export function Header(){
+export function Header({ children }){
+    const { signOut, user } = useAuth()
+    const navigate = useNavigate()
+
+    const avatarURL = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
+    function handleSignOut(){
+        navigate("/")
+        signOut()
+    }
     return(
         <Container>
             <h1>RocketMovies</h1>
 
-            <input 
-                type="text"
-                placeholder="Search by title"
-            />
+            <Search>
+                { children }
+            </Search>
 
             <Profile>
                 <div>
-                    <strong>Rafael Coelho</strong>
-                    <button type="button">exit</button>
+                    <strong>{user.name}</strong>
+                    <button type="button" onClick={ handleSignOut }>exit</button>
                 </div>
 
                 <Link to="/profile">
                     <img 
-                        src="https://github.com/raco1.png" 
+                        src={avatarURL} 
                         alt="User profile picture" 
                     />
                 </Link>
